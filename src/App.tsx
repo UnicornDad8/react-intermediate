92d6e4a542f84ade749cfeef32f12c6df1b6592b
UnicornDad8 <unicorndad8@gmail.com>
2023-05-11 16:34:08 -0400

    Mutating data on TodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import "./App.css";
-import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import "./App.css";
 
 function App() {
-  return <PostList />;
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
 }
 
 export default App;

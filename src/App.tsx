6c50df57bf6627210691a1a6494ae611c1ecefc9
UnicornDad8 <unicorndad8@gmail.com>
2023-05-17 13:03:07 -0400

    Working with zustand in the auth folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
-import { AuthProvider } from "./state-management/auth";
 import { TasksProvider } from "./state-management/tasks";
 import Counter from "./state-management/counter/Counter";
 import "./App.css";
 
 function App() {
   return (
-    <AuthProvider>
+    <>
       <TasksProvider>
         <Counter />
         <NavBar />
         <HomePage />
       </TasksProvider>
-    </AuthProvider>
+    </>
   );
 }
 

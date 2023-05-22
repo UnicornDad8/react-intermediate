167108763ad1f00bb1b929e489457a1096097b1a
UnicornDad8 <unicorndad8@gmail.com>
2023-05-22 08:50:54 -0400

    Layout routes

--- src/routing/UsersPage.tsx
+++ src/routing/UsersPage.tsx
@@ -1,12 +1,7 @@
-import { Navigate, Outlet } from "react-router-dom";
-import useAuth from "./hooks/useAuth";
+import { Outlet } from "react-router-dom";
 import UserList from "./UserList";
 
 const UsersPage = () => {
-  const { user } = useAuth();
-
-  if (!user) return <Navigate to="/login" />;
-
   return (
     <div className="row">
       <div className="col-6">

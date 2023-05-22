32f4ca011248d1897f4ed7f17b09cf54ca33ed53
UnicornDad8 <unicorndad8@gmail.com>
2023-05-22 08:41:55 -0400

    Private routes

--- src/routing/UsersPage.tsx
+++ src/routing/UsersPage.tsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import useAuth from "./hooks/useAuth";
 import UserList from "./UserList";
 
 const UsersPage = () => {
+  const { user } = useAuth();
+
+  if (!user) return <Navigate to="/login" />;
+
   return (
     <div className="row">
       <div className="col-6">
